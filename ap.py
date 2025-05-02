fae4771f710cd66e2c3411ade9564ba2f97119e2
ellajoshi <ellajoshiclasses@gmail.com>
2025-05-02 12:29:08 +0530

    Update ap.py

--- ap.py
+++ ap.py
@@ -1,4 +1,5 @@
 import streamlit as st
 st.title("hello world")
 name=st.text_input("enter your name")
-st.write("welcome"+name)
+age=int(st.text_input("enter your age"
+st.write("welcome"+name,age)
