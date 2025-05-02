48e1d2e26d3991328cf48595c1458346d3d9dabf
ellajoshi <ellajoshiclasses@gmail.com>
2025-05-02 12:33:48 +0530

    Update ap.py

--- ap.py
+++ ap.py
@@ -2,4 +2,7 @@ import streamlit as st
 st.title("hello world")
 name=st.text_input("enter your name")
 age=int(st.text_input("enter your age"))
-st.write("welcome"+name,age)
+if age>15 and age<=18:
+  st.write("welcome"+name,age,"you are a teen age kid")
+elif age<=15:
+  st.write("welcome"+name,age,"you are a  kid")
